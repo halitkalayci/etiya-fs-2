91f4a9b4ffaf326e7f591dec03a1eb23672e5c29
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-03 09:45:51 +0300

    03.10 9:45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'main-component', // ilgili component diğer htmller tarafından çağırılırken kullanılacak isim
+  templateUrl: './app.component.html', // ilgili componentin bağlı olduğu html dosyasının yolu
+  styleUrls: ['./app.component.css'], // ilgili componentin bağlı olduğu css dosyalarının yolu
+  //template: '<h3>Merhaba</h3>', // ilgili componentin render edeceği html
+  //styles: ['h3 {color:red}'], // ilgili componentin stilleri
 })
 export class AppComponent {
   title = 'etiya-fs-2';
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Angular'ın ayağa kaldırıldığı nokta.
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// SPA => Single Page Application
