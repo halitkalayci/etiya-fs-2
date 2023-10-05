70de93643ae31d9484ee1bec711d361d8d9e506b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-10-05 10:34:32 +0300

    interceptors intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import { DemoComponent } from './components/demo/demo.component';
 import { CssWorkshopComponent } from './pages/css-workshop/css-workshop.component';
 import { FirstWorkshopComponent } from './pages/first-workshop/first-workshop.component';
 import { PostListComponent } from './pages/post-list/post-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AddPostComponent } from './pages/add-post/add-post.component';
 import { InputErrorComponent } from './components/input-error/input-error.component';
 import { PostDetailComponent } from './pages/post-detail/post-detail.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,9 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     HttpClientModule,
     ReactiveFormsModule,
   ], // Bu modülün dışarıdan temin ettiği diğer modüller
-  providers: [], // Dependency Injection
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ], // Dependency Injection
   bootstrap: [AppComponent], // Uygulamanın başlayacağı ana componenti temsil eden yapı..
   exports: [], // Declarations alanındaki tanımlardan hangilerini dışarıya açacağımızı belirtir.
 })
